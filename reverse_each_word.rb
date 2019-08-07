a466ac28795498e9953cff05d7fbe565dd90a61d
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-07 12:10:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word (element)
   array = []
   reverse = []
   array = element.split (" ")
-  array.eachreverse <<  {|words| words.reverse}
+  array.each reverse <<  {|words| words.reverse}
   reverse.join (" ")
 end
